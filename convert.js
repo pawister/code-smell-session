081df40f88ef95074a0383adbd391194f913879e
pawister <phaphan.phawit@gmail.com>
2024-03-12 20:30:59 +0700

    Refactor convert function

--- convert.js
+++ convert.js
@@ -1,5 +1,8 @@
+// Converts an amount in US dollars to Russian rubles.
 function dollarsToRubles(dollars) {
-    let dollarsCount = dollars * 1.25;
-    return dollarsCount * 60;
+  const DOLLAR_INFLATION_RATE = 1.25;
+  const RUBLES_CONVERSION_RATE = 60;
+  return dollars * DOLLAR_INFLATION_RATE * RUBLES_CONVERSION_RATE;
 }
+
 module.exports = dollarsToRubles;
