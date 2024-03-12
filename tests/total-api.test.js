2285d80cf30006c8b39be376446a680ecdde62e4
pawister <phaphan.phawit@gmail.com>
2024-03-12 23:01:36 +0700

    Add post /total test

--- tests/total-api.test.js
+++ tests/total-api.test.js
@@ -1,5 +1,19 @@
+const request = require("supertest");
+const app = require("../app");
+
 describe("Sample Test", () => {
   it("should test that true === true", () => {
     expect(true).toBe(true);
+  });
+  it("POST / => Convert", () => {
+    return request(app)
+      .post("/total")
+      .send({
+        values: "1,2",
+      })
+      .expect("Content-Type", /json/)
+      .then((response) => {
+        expect(response.body).toEqual(225);
+      });
   });
 });
