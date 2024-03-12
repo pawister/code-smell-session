229db3932f787517602a68f089e4cd64d758f707
pawister <phaphan.phawit@gmail.com>
2024-03-12 19:42:11 +0700

    Remove console.log

--- routes/products.js
+++ routes/products.js
@@ -1,57 +1,43 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const createError = require('http-errors');
-const total = require('../total')
-const convert = require('../convert')
+const createError = require("http-errors");
+const total = require("../total");
+const convert = require("../convert");
 
 // Products Array
-const products = [{ id: '1', name: 'Playstation 5', inStock: false }];
+const products = [{ id: "1", name: "Playstation 5", inStock: false }];
 
-router.get('/', (req, res) => {
-    console.log("Get all products");
-    console.log("All products " + products.length);
-    console.log("Before return");
-    res.json(products);
+router.get("/", (req, res) => {
+  res.json(products);
 });
 
-router.get('/:id', (req, res, next) => {
-    console.log("Target id = " + req.params.id);
-    const product = products.find(
-        (product) => product.id === String(req.params.id)
-    );
-    console.log("Search result is " + product)
-    if (!product) {
-        console.log("Found nothing");
-        return next(createError(404, 'Not Found'));
-    }
-    console.log("Before return");
-    res.json(product);
+router.get("/:id", (req, res, next) => {
+  const product = products.find(
+    (product) => product.id === String(req.params.id)
+  );
+  if (!product) {
+    return next(createError(404, "Not Found"));
+  }
+  res.json(product);
 });
 
-router.post('/', (req, res, next) => {
-    console.log("Create new product");
-    const { body } = req;
-    if (typeof body.name !== 'string') {
-        return next(createError(400, 'Validation Error'));
-    }
-    console.log("Create new ID");
-    var newId = products.length + 1;
-    const newProduct = { id: "'" + newId + "'", name: body.name, inStock: false, };
-    products.push(newProduct);
-    console.log("Before return");
-    res.status(201).json(newProduct);
+router.post("/", (req, res, next) => {
+  const { body } = req;
+  if (typeof body.name !== "string") {
+    return next(createError(400, "Validation Error"));
+  }
+  var newId = products.length + 1;
+  const newProduct = { id: "'" + newId + "'", name: body.name, inStock: false };
+  products.push(newProduct);
+  res.status(201).json(newProduct);
 });
 
-router.post('/total', (req, res) => {
-    console.log("POST Total");
-    const { body } = req;
-    var values = body.values;
-    console.log("values " + values);
-    var result = total(values);
-    console.log("total " + result);
-    var convertResult = convert(result);
-    console.log("Before return");
-    res.json(convertResult);
+router.post("/total", (req, res) => {
+  const { body } = req;
+  var values = body.values;
+  var result = total(values);
+  var convertResult = convert(result);
+  res.json(convertResult);
 });
 
 module.exports = router;
