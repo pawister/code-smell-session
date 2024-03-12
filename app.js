90f25c9c368a55bf46712d265d3f65e3b8faf468
pawister <phaphan.phawit@gmail.com>
2024-03-12 23:10:33 +0700

    Add total route to app

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const express = require('express');
+const express = require("express");
 
-const productsRoute = require('./routes/products');
+const productsRoute = require("./routes/products");
+const productsRouteTotal = require("./routes/total");
 
 const app = express();
 
@@ -8,5 +9,6 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(express.json());
 
-app.use('/', productsRoute);
+app.use("/", productsRoute);
+app.use("/", productsRouteTotal);
 module.exports = app;
