2c09d2dd963ea4b0401dfc70a894f44c4b6eaf6d
pawister <phaphan.phawit@gmail.com>
2024-03-12 19:41:13 +0700

    Add test for all api

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,81 +1,76 @@
-const request = require('supertest');
-const app = require('../app');
+const request = require("supertest");
+const app = require("../app");
 
-describe('GET /', () => {
-    it('GET / => array of items', () => {
-        return request(app)
-            .get('/')
-            .expect('Content-Type', /json/)
-            .expect(200)
-            .then((response) => {
-                expect(response.body).toEqual(
-                    expect.arrayContaining([
-                        expect.objectContaining({
-                            id: expect.any(String),
-                            name: expect.any(String),
-                            inStock: expect.any(Boolean),
-                        }),
-                    ])
-                );
-            });
-    });
-    it('POST / => Convert', () => {
-        return (
-            request(app)
-                .post('/total')
-                .send({
-                    values: '1,2',
-                })
-                .expect('Content-Type', /json/)
-                .then((response) => {
-                    expect(response.body).toEqual(225);
-                })
+describe("GET /", () => {
+  it("GET / => array of items", () => {
+    return request(app)
+      .get("/")
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .then((response) => {
+        expect(response.body).toEqual(
+          expect.arrayContaining([
+            expect.objectContaining({
+              id: expect.any(String),
+              name: expect.any(String),
+              inStock: expect.any(Boolean),
+            }),
+          ])
         );
-    });
+      });
+  });
+  it("POST / => Convert", () => {
+    return request(app)
+      .post("/total")
+      .send({
+        values: "1,2",
+      })
+      .expect("Content-Type", /json/)
+      .then((response) => {
+        expect(response.body).toEqual(225);
+      });
+  });
 
+  it("GET / => items by ID", () => {
+    return request(app)
+      .get("/1")
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .then((response) => {
+        expect(response.body).toEqual(
+          expect.objectContaining({
+            id: expect.any(String),
+            name: expect.any(String),
+            inStock: expect.any(Boolean),
+          })
+        );
+      });
+  });
 
-    /*     it('GET / => items by ID', () => {
-            return request(app)
-                .get('/1')
-                .expect('Content-Type', /json/)
-                .expect(200)
-                .then((response) => {
-                    expect(response.body).toEqual(
-                        expect.objectContaining({
-                            id: expect.any(String),
-                            name: expect.any(String),
-                            inStock: expect.any(Boolean),
-                        })
-                    );
-                });
-        });
-    
-        it('GET /id => 404 if item not found', () => {
-            return request(app).get('/10000000000').expect(404);
-        });
-    
-        it('POST / => create NEW item', () => {
-            return (
-                request(app)
-                    .post('/')
-                    .send({
-                        name: 'Xbox Series X',
-                    })
-                    .expect('Content-Type', /json/)
-                    .expect(201)
-                    .then((response) => {
-                        expect(response.body).toEqual(
-                            expect.objectContaining({
-                                name: 'Xbox Series X',
-    
-                                inStock: false,
-                            })
-                        );
-                    })
-            );
-        });
-    
-        it('POST / => item name correct data type check', () => {
-            return request(app).post('/').send({ name: 123456789 }).expect(400);
-        }); */
+  it("GET /id => 404 if item not found", () => {
+    return request(app).get("/10000000000").expect(404);
+  });
+
+  it("POST / => create NEW item", () => {
+    return request(app)
+      .post("/")
+      .send({
+        name: "Xbox Series X",
+      })
+      .expect("Content-Type", /json/)
+      .expect(201)
+      .then((response) => {
+        expect(response.body).toEqual(
+          expect.objectContaining({
+            name: "Xbox Series X",
+
+            inStock: false,
+          })
+        );
+      });
+  });
+
+  it("POST / => item name correct data type check", () => {
+    return request(app).post("/").send({ name: 123456789 }).expect(400);
+  });
 });
