e88b89ee9e602bcc53f8218885d440d8cf19775f
pawister <phaphan.phawit@gmail.com>
2024-03-12 22:56:45 +0700

    Copy post /total request function to total.js file

--- routes/total.js
+++ routes/total.js
@@ -1,9 +1,16 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const createError = require('http-errors');
-const total = require('../total')
-const convert = require('../convert')
+const createError = require("http-errors");
+const total = require("../total");
+const convert = require("../convert");
 
 /*  */
+router.post("/total", (req, res) => {
+  const { body } = req;
+  var values = body.values;
+  var result = total(values);
+  var convertResult = convert(result);
+  res.json(convertResult);
+});
 
 module.exports = router;
