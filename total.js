e6e881f8191ffb12750a327bcf9a3f9f30b4c77a
pawister <phaphan.phawit@gmail.com>
2024-03-12 20:14:03 +0700

    Refactor total by extract 2 functions include checkIsAnIntegerIsInAValidRange and checkIfInteger

--- total.js
+++ total.js
@@ -1,19 +1,21 @@
 function total(numbers) {
-    var result = 0;
-    var parts = numbers.split(',');
-    for (var i = 0; i < parts.length; i++) {
-        var integer = parseInt(parts[i]);
-        //Check if integer
-        if (!isNaN(integer)) {
-            //Check is an integer is in a valid range
-            if (integer >= 0) {
-                if (integer <= 1000) {
-                    result += integer;
-                }
-            }
-        }
+  var result = 0;
+  var parts = numbers.split(",");
+  for (var i = 0; i < parts.length; i++) {
+    var integer = parseInt(parts[i]);
+    if (checkIfInteger(integer) && checkIsAnIntegerIsInAValidRange(integer)) {
+      result += integer;
     }
-    return result;
+  }
+  return result;
 }
+
+const checkIsAnIntegerIsInAValidRange = (integer) => {
+  return integer >= 0 && integer <= 1000;
+};
+
+const checkIfInteger = (integer) => {
+  return !isNaN(integer);
+};
 
 module.exports = total;
