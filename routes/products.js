9bb3b36b4aa9a3c59bdeaaff382fd849c6750968
Ruf <ruf@odds.team>
2024-03-12 13:41:56 +0700

    Restructure

--- routes/products.js
+++ routes/products.js
@@ -1,50 +1,35 @@
 const express = require('express');
-
 const router = express.Router();
-
 const createError = require('http-errors');
 
 // Products Array
-
 const products = [{ id: '1', name: 'Playstation 5', inStock: false }];
 
-// GET / => array of items
-
 router.get('/', (req, res) => {
+    console.log("Get all products");
+    console.log("All products " + products.length);
     res.json(products);
 });
 
-// GET / => items by ID
-
 router.get('/:id', (req, res, next) => {
+    console.log("Target id = " + req.params.id)
     const product = products.find(
         (product) => product.id === String(req.params.id)
     );
-
-    // GET /id => 404 if item not found
-
+    console.log("Search result is " + product)
     if (!product) {
         return next(createError(404, 'Not Found'));
     }
-
     res.json(product);
 });
 
 router.post('/', (req, res, next) => {
     const { body } = req;
-
     if (typeof body.name !== 'string') {
         return next(createError(400, 'Validation Error'));
     }
-
-    const newProduct = {
-        id: '1',
-
-        name: body.name,
-
-        inStock: false,
-    };
-
+    var newId = products.length + 1;
+    const newProduct = { id: "'" + newId + "'", name: body.name, inStock: false, };
     products.push(newProduct);
 
     res.status(201).json(newProduct);
